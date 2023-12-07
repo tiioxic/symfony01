24db0ce51223dfe5a726c01f153efb1c39b92484
tiioxic <toto27160@hotmail.fr>
2023-12-07 16:58:05 +0100

    utilisateur connexion

--- migrations/Version20231207111324.php
+++ migrations/Version20231207111324.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20231206150358 extends AbstractMigration
+final class Version20231207111324 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20231206150358 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE personne ADD tel VARCHAR(10) DEFAULT NULL, ADD adresse VARCHAR(255) DEFAULT NULL');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, prenom VARCHAR(20) NOT NULL, nom VARCHAR(20) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE personne DROP tel, DROP adresse');
+        $this->addSql('DROP TABLE user');
     }
 }
